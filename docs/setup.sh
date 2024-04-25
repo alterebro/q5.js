53635e6a8851f053e32d6abfd634c1f48fd50760
Jorge Moreno <alterebro@gmail.com>
2024-04-25 15:06:59 +0200

    Build /docs

--- docs/setup.sh
+++ docs/setup.sh
@@ -4,12 +4,12 @@
 curl https://p5js.org/offline-reference/p5-reference.zip > p5-reference.zip;
 unzip p5-reference.zip;
 mv p5-reference/assets assets
-rm -rf p5-reference
-rm p5-reference.zip
+# rm -rf p5-reference
+# rm p5-reference.zip
 
 # copy files over
-cp ../q5.js q5.js
-cp ../q5.min.js q5.min.js
+cp ../src/q5.js q5.js
+cp ../dist/q5.min.js q5.min.js
 cp ../tests.js tests.js
 cp ../README.md README.md
 
