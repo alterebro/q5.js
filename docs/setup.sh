c1f84dde76086e166e478e8e609c04a7ab253a4f
Jorge Moreno <alterebro@gmail.com>
2024-04-26 13:19:17 +0200

    Template UI Styling

--- docs/setup.sh
+++ docs/setup.sh
@@ -2,13 +2,15 @@
 
 # download p5 example assets
 curl https://p5js.org/offline-reference/p5-reference.zip > p5-reference.zip;
+# curl https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js > p5.js;
+curl https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js > p5.min.js;
 unzip p5-reference.zip;
 mv p5-reference/assets assets
 # rm -rf p5-reference
 # rm p5-reference.zip
 
 # copy files over
-cp ../src/q5.js q5.js
+# cp ../src/q5.js q5.js
 cp ../dist/q5.min.js q5.min.js
 cp ../tests.js tests.js
 cp ../README.md README.md
